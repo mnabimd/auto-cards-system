4b1c305b339026244a73909cc80ad2e0a65fe480
mnabimd <mnabimd@gmail.com>
2021-02-23 10:55:56 +0430

    Try/catch added to src-server

--- src-server/routers/card.js
+++ src-server/routers/card.js
@@ -17,11 +17,6 @@ const upload = multer({});
 const fs = require('fs');
 const path = require('path');
 
-
-const data = {
-    data: {}
-};
-
 router.get('/newCard', auth, (req, res) => {
     return sendRender('card', res);
 });
@@ -34,62 +29,36 @@ router.post('/newCard', auth, async (req, res) => {
 
     const kankorId = req.body.kankorId;
 
-    // Let's start a request to the 
-    const student = await axios.post(`http://localhost:4000/student/${kankorId}`, {
-        apiAuth: 'aGiftToMyFriend'
-    });
+    try {
+        // Let's start a request to the 
+        const student = await axios.post(`http://localhost:4000/student/${kankorId}`, {
+            apiAuth: 'aGiftToMyFriend.S.AB.HALIM'
+        });
 
-    const studentData = student.data.student;
+        const studentData = student.data.student;
 
-    if (!studentData) {
+        if (!studentData) {
+            return sendRender('card', res, {
+                message: 'No student found!'
+            })
+        }
+
         return sendRender('card', res, {
-            message: 'No student found!'
+            student: studentData
+        });
+
+    } catch (e) {
+        console.log([e, 'Location: Card.js'])
+        res.status(500).send({
+            error: e
         })
     }
 
-    return sendRender('card', res, {
-        student: studentData
-    })
-});
-
-// Live Preview :-
-router.get('/cardGenerate', (req, response) => {
-    // Let's create the PFD file and once done, let's send it:-
-
-    var html = fs.readFileSync(path.join(__dirname, '../../data/card-template/card.html'), 'utf8');
-    var options = {
-        base: "file:///C:/Users/Mohammad Nabi/Desktop/Web Projects/Web Development 2021/Auto Card System/data/card-template/",
-        width: '216mm',
-        height: '345mm',
-        quality: "100",
-        type: 'pdf'
-    };
-
-    pdf.create(html, options).toFile('data/card-generated/card.pdf', function (err, res) {
-        if (err) return console.log(err);
-
-        var i = 0;
-        i++;
-
-        console.log([res, i])
-        response.sendFile(path.join(__dirname, '../../data/card-generated/card.pdf'));
-    });
-
-
-    // This file the generated pdf file by PDF-HTML in data directory in the root.
-
-});
-
-
-router.get('/cardLive', (req, res) => {
-    res.send({
-        message: 'Cant be done here, dude!'
-    })
 });
 
 router.post('/cardLive', upload.single('profilePhoto'), (req, res) => {
 
-    function toBase64(arr) {
+    const toBase64 = (arr) => {
         //arr = new Uint8Array(arr) if it's an ArrayBuffer
         return `data:image/png;base64,${btoa(
            arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
@@ -118,29 +87,40 @@ router.post('/cardLive', upload.single('profilePhoto'), (req, res) => {
 router.post('/generatePDF', async (req, res) => {
     const values = req.body.values
 
-    let theHtmlTemplate = fs.readFileSync(path.join(__dirname, '../../data/card-template/card.html'), 'utf8');
+    try {
 
-    let newHtml = theHtmlTemplate.replace('<!-- CODE: MOHAMMDNABI -->', values);
+        let theHtmlTemplate = fs.readFileSync(path.join(__dirname, '../../data/card-template/card.html'), 'utf8');
 
-    const newHtmlName = uniqueString().slice(0, 10);
-    const newHTML = fs.writeFileSync(path.join(__dirname, `../../data/card-template/${newHtmlName}.html`), newHtml, () => {});
+        let newHtml = theHtmlTemplate.replace('<!-- CODE: MOHAMMDNABI -->', values);
 
-    // Now, let's convert this HTML file to PDF by PhantomJS or HTML_PDF:-
-    const readNewHTMLFile = fs.readFileSync(path.join(__dirname, `../../data/card-template/${newHtmlName}.html`), 'utf8');
+        const newHtmlName = uniqueString().slice(0, 10);
+        const newHTML = fs.writeFileSync(path.join(__dirname, `../../data/card-template/${newHtmlName}.html`), newHtml, () => {});
 
-    var options = {
-        base: "file:///C:/Users/Mohammad Nabi/Desktop/Web Projects/Web Development 2021/Auto Card System/data/card-template/",
-        width: '216mm',
-        height: '344.5mm',
-        quality: "100",
-        type: 'pdf'
-    };
+        // Now, let's convert this HTML file to PDF by PhantomJS or HTML_PDF:-
+        const readNewHTMLFile = fs.readFileSync(path.join(__dirname, `../../data/card-template/${newHtmlName}.html`), 'utf8');
 
-    pdf.create(readNewHTMLFile, options).toFile(`data/card-generated/${newHtmlName}.pdf`, function (err, response) {
-        if (err) return console.log(err);
-    });
-    
-    res.send('Done!')
+        var options = {
+            base: "file:///C:/Users/Mohammad Nabi/Desktop/Web Projects/Web Development 2021/Auto Card System/data/card-template/",
+            width: '216mm',
+            height: '344.5mm',
+            quality: "100",
+            type: 'pdf'
+        };
+
+        pdf.create(readNewHTMLFile, options).toFile(`data/card-generated/${newHtmlName}.pdf`, function (err, response) {
+            if (err) return console.log(err);
+        });
+
+        // Send the response:-
+        res.send({
+            message: 'Card generated and saved!',
+            path: path.join(__dirname, `../../data/card-generated/${newHtmlName}.pdf`)
+        });
+
+    } catch (e) {
+        console.log([e, 'Location: card.js'])
+    }
+
 });
 
 
